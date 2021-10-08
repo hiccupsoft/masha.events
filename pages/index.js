f28c1c79853ae33469b9df6447a91502a69f5f08
hiccup1991 <hiccup1991@hotmail.com>
2021-10-08 13:29:25 -0400

    update page

--- pages/index.js
+++ pages/index.js
@@ -18,27 +18,32 @@ const Home = () => {
           <div className="main-wrapper">
             <div className="main-header-wrap">
               <h1>The best parties on The Rock</h1>
-              <p>We are just starting to create amazing parties on Malta. This will be a nice journey, come to take this journey with us and visit our parties. Enjoy Life ! </p>
+              <p>We’ve been throwing amazing parties since 2010 porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo. </p>
             </div>
             <img src="static/images/wave.png" alt="" className="wave-img" />
+
             <div className="main-banner">
-              <img src="static/images/main-section.svg" alt="" className="banner-2" />
+              <img src="static/images/firework.png" alt="" className="fireworks" />
+              <div className="mobile-banner">
+                <img src="static/images/main-section.png" alt="" className="banner-2" />
+              </div>
             </div>
           </div>
         </div>
       </header>
+
       <section className="main-section-wrapper">
         <div className="container">
           <div className="card-wrapper-section">
             <div className="details-wrapper">
-              <img src="static/images/card-1.svg" alt="" className="card-1-img" />
+              <img src="static/images/card-2.jpg" alt="" className="card-1-img" />
             </div>
             <div className="card-details-wrapper">
               <h2>Featured party</h2>
-              <div className="card-info">
+              <div className="card-info" >
                 <div className="card_header">
                   <div className="card_title">
-                    <h3>Russian Night with DJ Liana </h3>
+                    <h3>Russian Night with DJ Liana. </h3>
                     <p>Friday, Oct 15, 2020  |  21:00 - 03:00</p>
                   </div>
                   <button> €35.00 to  €410.00</button>
@@ -48,18 +53,19 @@ const Home = () => {
                     Dancers, competitions and more!
                     Tickets available now!</p>
                   <button onClick={handleClick}>
-                    <a> View details </a> </button>
+                    <a> View details </a>
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-          <div className="card-wrapper-section">
+          <div className="card-wrapper-section" id="mobile-card">
             <div className="card-details-wrapper_1">
               <h2 className="right-title">Featured party</h2>
-              <div className="card-info">
+              <div className="card-info" >
                 <div className="card_header">
                   <div className="card_title">
-                    <h3>Russian Night with DJ Liana </h3>
+                    <h3>Russian Night with DJ Liana. </h3>
                     <p>Friday, Oct 15, 2020  |  21:00 - 03:00</p>
                   </div>
                   <button> €35.00 to  €410.00</button>
@@ -74,28 +80,35 @@ const Home = () => {
               </div>
             </div>
             <div className="details-wrapper-1">
-              <img src="static/images/card-2.svg" alt="" className="card-2-img" />
+              <img src="static/images/card-1.png" alt="" className="card-2-img" />
             </div>
           </div>
+
         </div>
+
       </section>
+
       <footer className="footer-section">
         <div className="container">
           <div className="gallary-wrapper">
             <div className="footer-gallary">
-              <img src="static/images/gallary-1.svg" alt="" />
-              <img src="static/images/gallary-2.svg" alt="" />
-              <img src="static/images/gallary-3.svg" alt="" />
+              <img src="static/images/gallary.png" alt="" />
+              <img src="static/images/gallary-2.png" alt="" />
+              <img src="static/images/gallary-3.png" alt="" />
             </div>
             <div className="follow-btn">
               <img src="static/images/instagram.svg" alt="" />
-              <a href>Follow us on Instagram</a>
+              <a href="">Follow us on Instagram  </a>
             </div>
+
           </div>
+
           <div className="copyright">
             <p>© Masha Events 2021. All Rights Reserved.</p>
           </div>
+
         </div>
+
       </footer>
     </div>
   )
--- pages/russian-night-with-dj-liana.js
+++ pages/russian-night-with-dj-liana.js
@@ -14,7 +14,7 @@ const RussianNightWithDJLiana = (props) => {
                     </div>
                     <div className="details-inner-section">
                         <div className="home-page">
-                            <i className="fas fa-arrow-left" />
+                            <i className="fas fa-arrow-left"></i>
                             <a onClick={handleBackToHome}>Back to home</a>
                         </div>
                         <div className="details-view-wrapper">
@@ -28,14 +28,16 @@ const RussianNightWithDJLiana = (props) => {
                                 <a href="https://www.chitzo.com/Events/ViewEvent/whatson-pembroke-night-clubs-russian-night.42503" target="_blank"><button className="book-now">Book now</button></a>
                                 <p>All sales are final. Once booked, tickets can not be transferred or refunded.</p>
                                 <div className="booking-card-footer">
-                                    <a href>Terms &amp; conditions</a>
-                                    <i className="fas fa-chevron-right" />
+                                    <a href="">Terms &amp; conditions</a>
+                                    <i className="fas fa-chevron-right"></i>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </header>
+
             <section className="venue-details-section">
                 <div className="container">
                     <div className="venue-details-wrapper">
@@ -44,66 +46,76 @@ const RussianNightWithDJLiana = (props) => {
                             <p>{props.title}. <br />
                                 The best of Russian music, treats and of course alcohol with DJ Liana. Dancers, competitions and
                                 more! </p>
-                            <p>
+                            <p className="ticket-para">
                                 Tickets available below! <br /> - 35 Euro Entrance Fee <br /> - 200 Euro Minimum Spend for a V.I.P
                                 Table
                             </p>
-                        </div>
-                        <div className="map">
-                            <h1>Venue</h1>
-                            {/* <img src="static/images/location.svg" alt=""> */}
-                            <iframe src={props.location} width="400px" height="357px" style={{ border: 0 }} allowFullScreen loading="lazy" />
-                        </div>
-                    </div>
-                    <div className="venue-details-wrap">
-                        <div className="address-wrapper">
-                            <h1>Have questions?</h1>
+
+                            <h1 className="question">Have questions?</h1>
                             <p> We would love to answer all your questions. Just email our team at </p>
                             <p>
                                 <a href="info@groovemedia.eu"> info@groovemedia.eu </a> and we’ll get back to you asap.
                             </p>
                         </div>
-                        <div className="address-wrapper address-flex">
-                            <h1>{props.address1}</h1>
-                            <span>{props.address2}</span>
-                            <div className="social-group">
-                                <h1>Share with friends</h1>
-                                <div className="social-icons">
-                                    <img src="static/images/facebook.svg" alt="" />
-                                    <img src="static/images/google.svg" alt="" />
-                                    <img src="static/images/twitter.svg" alt="" />
-                                    <img src="static/images/file.svg" alt="" />
+                        <div className="map">
+                            <h1 className="venue-title">Venue</h1>
+                            {/* <img src="static/images/location.svg" alt=""> */}
+                            <div className="location-iframe">
+                                <iframe src={props.location} width="400px" height="357px" style={{ border: 0 }} allowFullScreen loading="lazy"></iframe>
+                            </div>
+
+
+                            <div className="address-wrapper address-flex">
+                                <h1 className="location-heading">{props.address1}</h1>
+                                <span>{props.address2}</span>
+                                <div className="social-group">
+                                    <h1>Share with friends</h1>
+                                    <div className="social-icons">
+                                        <img src="static/images/facebook.svg" alt="" />
+                                        <img src="static/images/google.svg" alt="" />
+                                        <img src="static/images/twitter.svg" alt="" />
+                                        <img src="static/images/file.svg" alt="" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
+
             </section>
+
+
+
             <footer className="footer-section">
                 <div className="container">
                     <div className="gallary-wrapper">
                         <div className="footer-gallary">
-                            <img src="static/images/gallary-1.svg" alt="" />
-                            <img src="static/images/gallary-2.svg" alt="" />
-                            <img src="static/images/gallary-3.svg" alt="" />
+                            <img src="static/images/gallary.png" alt="" />
+                            <img src="static/images/gallary-2.png" alt="" />
+                            <img src="static/images/gallary-3.png" alt="" />
                         </div>
                         <div className="follow-btn">
                             <img src="static/images/instagram.svg" alt="" />
-                            <a href>Follow us on Instagram </a>
+                            <a href="">Follow us on Instagram </a>
                         </div>
+
                     </div>
+
                     <div className="copyright">
                         <p>© Masha Events 2021. All Rights Reserved.</p>
                     </div>
+
                 </div>
+
             </footer>
         </div>
     )
 }
 
-export const getServerSideProps = async ({req, res, resolvedUrl}) => {
+export const getServerSideProps = async ({ req, res, resolvedUrl }) => {
     console.log(process.env.NEXT_PUBLIC_APP_URL);
-    
+
     const url = resolvedUrl.replace("/", "");
     const ret = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/events/${url}`);
     const data = await ret.json();
